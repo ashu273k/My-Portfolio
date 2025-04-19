f36ac955ff66eee74f9fc0df28e796f2fc19620b
ashu273k <ashu9472.as@gmail.com>
2025-04-19 16:50:34 +0530

    Basic configurations for tailwind CSS, Three js and grasp�. Also I have added the assests�.

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <h1 className='text-3xl underline'>Welcome to my 3D Portfolio!</h1>
+    </div>
+  )
+}
+
+export default App
