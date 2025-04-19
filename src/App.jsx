efa05386a76f012d51f683a498babfc8366ea5c5
ashu273k <ashu9472.as@gmail.com>
2025-04-19 22:17:24 +0530

    Added Hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
+import Hero from './sections/Hero'
 
 function App() {
   return (
-    <div>
-      <h1 className='text-3xl underline'>Welcome to my 3D Portfolio!</h1>
-    </div>
+    <>
+      <Hero />
+    </>
   )
 }
 
 export default App
-c
